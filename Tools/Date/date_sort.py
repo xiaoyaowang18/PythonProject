071f97ce5d24ce609b652a4e76bcef38bfd86223
= <=>
2021-11-04 13:33:59 +0800

    '节假日日期'

--- Tools/Date/date_sort.py
+++ Tools/Date/date_sort.py
@@ -11,6 +11,8 @@ from chinese_calendar import is_workday
 from chinese_calendar import is_holiday
 from Tools.Date.getAllDayOfYear import getALLDayOfYear
 import chinese_calendar as calendar
+import openpyxl
+import pandas as pd
 
 holidays = {
     "New Year's Day": "元旦",
@@ -36,18 +38,33 @@ def day_sort(date):
     holiday_name = calendar.get_holiday_detail(date)
     date_dict = {}
     date_dict['date'] = date.strftime('%Y-%m-%d')
-    date_dict['工作日'] = on_workday
-    date_dict['休息日'] = on_holiday
+    date_dict['is_workday'] = on_workday
+    date_dict['is_holiday'] = on_holiday
     if holiday_name[1] is not None:
-        date_dict['节假日'] = holidays[holiday_name[1]]
+        date_dict['holiday'] = holidays[holiday_name[1]]
+    else:
+        date_dict['holiday'] = None
     return date_dict
 
 
 if __name__ == '__main__':
-    years_list = [2016, 2017, 2018, 2019, 2020, 2021]
+    years_list = [2019, 2020, 2021]
     dates_list = getALLDayOfYear(years_list)
-    allday = []
+    excel_path = r"C:\Users\78122\Desktop\rq.xlsx"
+    all_date = []
+    all_is_workday = []
+    all_is_holiday = []
+    all_holiday = []
+    dict = {}
     for date in dates_list:
         daysort = day_sort(date)
-        allday.append(daysort)
-    print(allday)
+        all_date.append(daysort['date'])
+        all_is_workday.append(daysort['is_workday'])
+        all_is_holiday.append(daysort['is_holiday'])
+        all_holiday.append(daysort['holiday'])
+    dict['date'] = all_date
+    dict['is_workday'] = all_is_workday
+    dict['is_holiday'] = all_is_holiday
+    dict['holiday'] = all_holiday
+    df = pd.DataFrame(dict)
+    df.to_excel(excel_path, index=False)
